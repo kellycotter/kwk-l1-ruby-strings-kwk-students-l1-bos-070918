2eefca4438e8aeb844580cc8105fa8005e5d2274
Kelly Cotter <kjc853@gmail.com>
2018-07-10 14:03:12 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -7,10 +7,20 @@ puts "What is your name?"
 
 guest_name=gets.chomp.capitalize
 
+puts "What is the name of the party?"
 
+party_name=gets.chomp.capitalize
 
+puts "When is the party?"
 
+date=gets.chomp.capitalize
 
+puts= "What time is the party?"
 
+time=gets.chomp.capitalize
 
-puts "Dear #{guest_name},you are cordially invited to"
+puts "Who is the host of the party?"
+
+host_name=gets.chomp.capitalize
+
+puts "Dear #{guest_name},you are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than October 30. Sincerely, #{host_name}."
