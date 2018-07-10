20d6274f8512cff95f9dc26d65161e95f94e281a
Kelly Cotter <kjc853@gmail.com>
2018-07-10 13:56:28 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,3 +2,15 @@
 
 # Try starting out with puts'ing a string.
 
+
+puts "What is your name?"
+
+guest_name=gets.chomp.capitalize
+
+
+
+
+
+
+
+puts "Dear #{guest_namename},you are cordially invited to"
